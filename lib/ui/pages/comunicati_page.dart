24f3e28e91194e4d25346f540d4698d32eb0a5e5
Antonio Napolitano <nap@napaalm.xyz>
2020-09-09 18:40:09 +0200

    Add lazy loading and pdf viewer to comunicati page

--- lib/ui/pages/comunicati_page.dart
+++ lib/ui/pages/comunicati_page.dart
@@ -17,26 +17,139 @@
  * along with davapp.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
+import 'package:davapp/backend/api.dart';
+import 'package:http/http.dart' as http;
+import 'package:native_pdf_view/native_pdf_view.dart';
+
+enum ComunicatiType {
+  studenti,
+  genitori,
+  docenti,
+  salvati,
+}
+
+class ComunicatoView extends StatelessWidget {
+  final Future<PdfDocument> pdfDocumentFuture;
+
+  ComunicatoView(this.pdfDocumentFuture);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Comunicato'),
+      ),
+      body: PdfView(
+        controller: PdfController(
+          document: this.pdfDocumentFuture,
+        ),
+      ),
+    );
+  }
+}
+
+class LazyComunicatiGenerator {
+  final RegExp nameRegExp = RegExp(r'^([0-9]*)-(.*)\.pdf');
+  final DateFormat dateFormat = DateFormat.yMMMMd('it_IT').add_Hm();
+
+  final Function(int) comunicatiGetter;
+  List<Comunicato> comunicatiCache;
+
+  LazyComunicatiGenerator(this.comunicatiGetter) {
+    updateCache(20);
+  }
+
+  void updateCache([int number]) async {
+    this.comunicatiCache = await this.comunicatiGetter(number);
+  }
+
+  Widget getComunicato(BuildContext context, int index) {
+    if (index == 0) updateCache();
+    try {
+      var comunicato = comunicatiCache[index];
+      return Card(
+        child: ListTile(
+          leading: AspectRatio(
+            aspectRatio: 1.0,
+            child: Center(
+              child: Text(
+                this.nameRegExp.firstMatch(comunicato.nome)?.group(1) ?? '?',
+                style: DefaultTextStyle.of(context)
+                    .style
+                    .apply(fontSizeFactor: 1.5),
+              ),
+            ),
+          ),
+          title: Text(
+            this
+                    .nameRegExp
+                    .firstMatch(comunicato.nome ?? '')
+                    ?.group(2)
+                    ?.replaceAll('_', ' ') ??
+                '?',
+          ),
+          subtitle:
+              Text(dateFormat.format(comunicato.data) ?? 'qualche tempo fa...'),
+          trailing: IconButton(
+            icon: Icon(Icons.save_alt),
+            onPressed: () {},
+          ),
+          isThreeLine: true,
+          enabled: true,
+          onTap: () async {
+            http.Response response = await http.get(comunicato.url ??
+                'https://www.antonionapolitano.eu/file_not_found.pdf');
+            await Navigator.push(
+                context,
+                PageRouteBuilder(
+                  pageBuilder: (BuildContext context, _, __) =>
+                      ComunicatoView(PdfDocument.openData(response.bodyBytes)),
+                ));
+          },
+        ),
+      );
+    } on RangeError {
+      return null;
+    }
+  }
+}
+
+AppBar comunicatiBar(String group) => AppBar(
+      title: Text('Comunicati ' + group),
+    );
 
 class ComunicatiPage extends StatefulWidget {
-  ComunicatiPage({Key key}) : super(key: key);
+  LazyComunicatiGenerator lazyComunicatiGenerator;
+
+  final Map<ComunicatiType, Function(int)> comunicatiGetter = {
+    ComunicatiType.studenti: APIDav.instance.comunicatiStudenti,
+    ComunicatiType.genitori: APIDav.instance.comunicatiGenitori,
+    ComunicatiType.docenti: APIDav.instance.comunicatiDocenti,
+  };
+
+  ComunicatiPage(ComunicatiType comunicatiType, {Key key}) : super(key: key) {
+    this.lazyComunicatiGenerator =
+        LazyComunicatiGenerator(this.comunicatiGetter[comunicatiType]);
+  }
 
   @override
   _ComunicatiPageState createState() => _ComunicatiPageState();
 }
 
 class _ComunicatiPageState extends State<ComunicatiPage> {
+  Future<void> refreshList() async {
+    await widget.lazyComunicatiGenerator.updateCache();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          Placeholder(
-            color: Colors.red,
-          ),
-        ],
+    return RefreshIndicator(
+      onRefresh: refreshList,
+      child: ListView.builder(
+        itemBuilder: widget.lazyComunicatiGenerator.getComunicato,
       ),
     );
   }
